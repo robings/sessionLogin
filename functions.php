b3521fa8c7ddb2f8b6f1ae07d1a1d3e3b25f238a
robings <r_gordon_stables@hotmail.com>
2020-01-28 21:45:22 +0000

    added doc blocks

--- functions.php
+++ functions.php
@@ -1,9 +1,17 @@
 <?php
 
-function isUserLoggedIn() {
+/**
+ * check if user is logged on
+ * @return bool
+ */
+function isUserLoggedIn(): bool {
     return (isset($_SESSION['loggedIn']));
 }
 
+/**
+ * check if there is a message to display and return a human readable version
+ * @return string
+ */
 function checkForMessage() {
     if (isset($_GET['loginMessage'])) {
         if ($_GET['loginMessage'] == 'invalidInput') {
@@ -16,7 +24,12 @@ function checkForMessage() {
     }
 }
 
-function checkUsername($username): bool {
+/**
+ * check if the username is correct
+ * @param string $username - the username to compare against
+ * @return bool
+ */
+function checkUsername(string $username): bool {
   if (isset($_POST['username'])) {
       if ($_POST['username'] == $username) {
           return true;
@@ -26,7 +39,12 @@ function checkUsername($username): bool {
   }
 }
 
-function checkPassword($password): bool {
+/**
+ * check if password is correct
+ * @param string $password - the password to compare against
+ * @return bool
+ */
+function checkPassword(string $password): bool {
     if (isset($_POST['password'])) {
         if ($_POST['password'] == $password) {
             return true;
@@ -36,7 +54,12 @@ function checkPassword($password): bool {
     }
 }
 
-function validateInput($input): bool {
+/**
+ * validate the input for illegal characters
+ * @param string $input - the input to validate
+ * @return bool
+ */
+function validateInput(string $input): bool {
     if ((strpos($input, '!') !== false) || (strpos($input, '&') !== false) || (strpos($input, '<') !== false) || (strpos($input, '>') !== false) || (strpos($input, ' ') !== false)) {
         return true;
     } else {
@@ -44,13 +67,12 @@ function validateInput($input): bool {
     }
 }
 
-
-
-function setLoggedIn() {
-    //if username and password checkout, set a flag for logged in the session
-}
-
-function logOut($location, $message = '') {
+/**
+ * function to logout user
+ * @param string $location - the location to redirect to
+ * @param string $message - reference that is converted to message by checkForMessage function
+ */
+function logOut(string $location, string $message = '') {
         if ($message != '') {
         $message = '?' . $message;
     }
