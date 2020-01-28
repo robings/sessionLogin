c098b3d294497a0579d74c47d9d6ac7385611222
robings <r_gordon_stables@hotmail.com>
2020-01-28 13:25:35 +0000

    created files and added basic form to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once ('functions.php');
+
+
+?>
+
+<html>
+<h1>Login</h1>
+<form action='account.php' method='post'>
+    <label>Username: </label>
+    <input type='text' name='username' />
+    <br /><br />
+    <label>Password: </label>
+    <input type='text' name='password' />
+    <br /><br />
+    <input type='submit' />
+
+</form>
+
+</html>
