3662cbe7267d300f563d102a31f58bd93b4d75a7
robings <r_gordon_stables@hotmail.com>
2020-01-28 21:11:00 +0000

    changed password input to password type

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isUserLoggedIn()) {
     <input type='text' name='username' />
     <br /><br />
     <label>Password: </label>
-    <input type='text' name='password' />
+    <input type='password' name='password' />
     <br /><br />
     <input type='submit' />
 
